c2a30febf4c84f35efd4d960b58fbc003263b8d1
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-17 16:17:09 +0100

    ADDED - component service + props

--- src/components/AppMainServices.vue
+++ src/components/AppMainServices.vue
@@ -1,110 +1,58 @@
+<script>
+import ServiceItem from './ServiceItem.vue';
+
+export default {
+    components: {
+        ServiceItem,
+    },
+    data() {
+        return {
+            services: [
+                {
+                    imgSrc: "/img/first-section-img-1.png",
+                    flexReverse: false,
+                    category: "Business Growth",
+                    title: "Increase Brand Awareness",
+                    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima accusamus incidunt at veniam aliquam.",
+                    consultationLink: "#"
+                },
+
+                {
+                    imgSrc: "/img/first-section-img-2.png",
+                    flexReverse: true,
+                    category: "Investors in People",
+                    title: "In-House Sales Training",
+                    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima accusamus incidunt at veniam aliquam.",
+                    consultationLink: "#"
+                },
+
+                {
+                    imgSrc: "/img/first-section-img-3.png",
+                    flexReverse: false,
+                    category: "Social Media Analysis",
+                    title: "Harness Your Social Proof",
+                    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima accusamus incidunt at veniam aliquam.",
+                    consultationLink: "#"
+                },
+
+            ]
+        };
+    }
+};
+</script>
+
+
 <template>
-    <div class="container">
-        <section class="service">
-            <div class="row">
-                <div class="col-6">
-                    <figure class="img-wrapper">
-                        <img src="/img/first-section-img-1.png" alt="">
-                    </figure>
-                </div>
-                <div class="col-6">
-                    <div class="service-card">
-                        <div class="details-card"></div>
-                        <p>Business Growth</p>
-                        <h2 class="card-title">Increase Brand Awerness</h2>
-                        <p class="card-text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi
-                            aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima
-                            accusamus incidunt at veniam aliquam?
-                        </p>
+    <section class="services">
+        <div class="container">
+            <ServiceItem v-for="(service, index) in services" :key="index" :service="service" />
+        </div>
 
-                        <div class="button btn-orange">
-                            <a href="#">Get a Consultation</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="service">
-            <div class="row flex-reverse">
-                <div class="col-6">
-                    <figure class="img-wrapper">
-                        <img src="/img/first-section-img-2.png" alt="">
-                    </figure>
-                </div>
-                <div class="col-6">
-                    <div class="service-card">
-                        <div class="details-card"></div>
-                        <p>Investors in People</p>
-                        <h2 class="card-title">In-House Sales Training</h2>
-                        <p class="card-text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi
-                            aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima
-                            accusamus incidunt at veniam aliquam?
-                        </p>
-
-                        <div class="button btn-orange">
-                            <a href="#">Get a Consultation</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="service">
-            <div class="row">
-                <div class="col-6">
-                    <figure class="img-wrapper">
-                        <img src="/img/first-section-img-3.png" alt="">
-                    </figure>
-                </div>
-                <div class="col-6">
-                    <div class="service-card">
-                        <div class="details-card"></div>
-                        <p>Social Media Analysis</p>
-                        <h2 class="card-title">Harness Your Social Proof</h2>
-                        <p class="card-text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sint veritatis, hic excepturi
-                            aperiam saepe fugit officia suscipit modi, accusantium voluptatem? Quia tempora quam minima
-                            accusamus incidunt at veniam aliquam?
-                        </p>
-
-                        <div class="button btn-orange">
-                            <a href="#">Get a Consultation</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-    </div>
+    </section>
 </template>
 
 <style scoped>
-.service {
-    padding-top: 100px;
-}
-
-.service-card {
-    width: 70%;
-    margin: 0 auto;
-    padding: 100px 0;
-}
-
-.button {
-    width: max-content;
-}
-
-.card-title {
-    font-size: 44px;
-    margin-bottom: 28px;
-    margin-top: 28px;
-}
-
-.card-text {
-    font-size: 17px;
-    margin-bottom: 28px;
-    line-height: 30px;
+.services {
+    padding-top: 50px;
 }
 </style>
---
+++ src/components/ServiceItem.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    props: {
+        service: Object
+    }
+};
+</script>
+
+<template>
+    <div class="service-item" :class="{ 'flex-reverse': service.flexReverse }">
+        <div class="row">
+            <div class="col-6">
+                <figure class="img-wrapper">
+                    <img :src="service.imgSrc">
+                </figure>
+            </div>
+            <div class="col-6">
+                <div class="service-card">
+                    <p>{{ service.category }}</p>
+                    <h2 class="card-title">{{ service.title }}</h2>
+                    <p class="card-text">{{ service.description }}</p>
+
+                    <div class="button btn-orange">
+                        <a :href="service.consultationLink">Get a Consultation</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.service-card {
+    width: 70%;
+    margin: 0 auto;
+    padding: 100px 0;
+}
+
+.button {
+    width: max-content;
+}
+
+.card-title {
+    font-size: 44px;
+    margin-bottom: 28px;
+    margin-top: 28px;
+}
+
+.card-text {
+    font-size: 17px;
+    margin-bottom: 28px;
+    line-height: 30px;
+}
+
+.service-item {
+    padding-top: 100px;
+}
+</style>
